de1e72b1da33d7cd9b32aced4232b20f30d67b03
jnpr-juntaod <juntaod@juniper.net>
2015-02-04 00:58:03 -0800

    merge

--- project/Build.scala
+++ project/Build.scala
@@ -34,7 +34,6 @@ object Build extends Build {
       "net.juniper"         %% "easy-rest-core" % "0.1.3"           withSources(),
       "net.juniper"         %% "easy-rest-persistence" % "0.1.3"    withSources(),
       "net.juniper"         %% "easy-rest-orm" % "0.1.3"            withSources(),
-      "net.juniper"         % "jnc-library" % "0.1.3"               withSources(),
       "ch.qos.logback"      %   "logback-classic" % "1.1.2"         withSources(),
       "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"     withSources()
     ),

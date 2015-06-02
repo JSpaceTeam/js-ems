17360e2961a54f28e1505b31ced4ea3d12c2ef56
jnpr-juntaod <juntaod@juniper.net>
2015-06-02 22:45:07 +0800

    fixed compilation error

--- ems-boot/src/main/scala/net/juniper/ems/EmsBoot.scala
+++ ems-boot/src/main/scala/net/juniper/ems/EmsBoot.scala
@@ -2,18 +2,17 @@ package net.juniper.ems
 
 import net.juniper.easyrest.boot.Bootstrap
 import net.juniper.easyrest.core.EasyRestActor
-import net.juniper.easyrest.persistence.DatabaseSupport
 import net.juniper.easyrest.subsystem.EasyRestIntegrationSubsystem
 import net.juniper.ems.notifications.EmsNotifications
-import net.juniper.yang.EmsServerAllRoutes
 
 object EmsBoot extends App {
   new EmsBootstrap().start()
 }
 
-class EmsBootstrap extends Bootstrap[EmsActor] with DatabaseSupport with EasyRestIntegrationSubsystem with EmsNotifications
+class EmsBootstrap extends Bootstrap[EmsActor] with EasyRestIntegrationSubsystem with EmsNotifications
 
-//This actor should be generated from Yang
-class EmsActor extends EasyRestActor with EmsServerAllRoutes {
-  def getRoute = emsServerAllRoutes
+class EmsActor extends EasyRestActor {
+  def getRoute = get {
+    complete("")
+  }
 }
